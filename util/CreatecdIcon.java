f8636ef404a9a63ab0cf409df9e80721ea13adcc
a2448487564 <37462964+a2448487564@users.noreply.github.com>
2018-04-20 19:51:18 +0800

    Create CreatecdIcon.java

---
+++ util/CreatecdIcon.java
@@ -0,0 +1,17 @@
+package com.wyp.util;
+
+import java.net.URL;
+
+
+import javax.swing.ImageIcon;
+
+import com.wyp.main.Library;
+
+
+public class CreatecdIcon {
+	public static ImageIcon add(String ImageName){
+		URL IconUrl = Library.class.getResource("/"+ImageName);
+		ImageIcon icon=new ImageIcon(IconUrl);
+		return icon;
+	}
+}
